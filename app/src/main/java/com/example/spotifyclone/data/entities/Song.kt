4f7a90e2b98b20b7e2aaf4244fe24e0276a5f58f
Sumitk7970 <kumarsumit8210@gmail.com>
2022-04-28 23:30:02 +0530

    First commit

---
+++ app/src/main/java/com/example/spotifyclone/data/entities/Song.kt
@@ -0,0 +1,8 @@
+package com.example.spotifyclone.data.entities
+
+data class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val imageUrl: String = "",
+    val songUrl: String = ""
+)
